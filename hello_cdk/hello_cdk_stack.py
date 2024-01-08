b508f92f7128873c3c917ff60151ec08404af49b
Maxwell Ward <maxward4@gmail.com>
2024-01-08 11:47:04 -0500

    cdk destroy deletes related s3 bucket on stack deletion

--- hello_cdk/hello_cdk_stack.py
+++ hello_cdk/hello_cdk_stack.py
@@ -1,6 +1,6 @@
+import aws_cdk as cdk
 from aws_cdk import Stack
 from aws_cdk import aws_s3 as s3
-
 # aws_sqs as sqs,
 from constructs import Construct
 
@@ -10,8 +10,7 @@ class HelloCdkStack(Stack):
         super().__init__(scope, construct_id, **kwargs)
 
         # The code that defines your stack goes here
-        bucket = s3.Bucket(self, "MyFirstBucket", versioned=True)
-
+        bucket = s3.Bucket(self, "MyFirstBucket", versioned=True, removal_policy=cdk.RemovalPolicy.DESTROY, auto_delete_objects=True)
         # example resource
         # queue = sqs.Queue(
         #     self, "HelloCdkQueue",
